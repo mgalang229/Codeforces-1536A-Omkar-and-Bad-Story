aca9109f649e8482f83c7846ec52a23cc073f926
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-06-08 12:33:42 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		bool negative = false;
+		for (int i = 0; i < n; i++) {
+			int x;
+			cin >> x;
+			if (x < 0) {
+				// if there is a negative value, set 'negative' to true
+				negative = true;
+			}
+		}
+		if (negative) {
+			// output is NO if there is negative value
+			cout << "NO\n";
+			// continue the loop
+			continue;
+		}
+		// otherwise, it is always POSSIBLE to create a "nice" array
+		cout << "YES\n";
+		// print 101 elements since we will numbers from 0 to 100 (inclusive)
+		cout << 101 << '\n';
+		for (int i = 0; i <= 100; i++) {
+			cout << i << " ";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
